e33694a59222437a206a6a6eea7e85bb96711942
LexHeadko <n-odes11rus@yandex.ru>
2023-02-27 00:55:55 +0200

    5 commit

--- src/Transport/Transport.java
+++ src/Transport/Transport.java
@@ -4,7 +4,7 @@ public abstract class Transport {
     private final int wheelsCount;
     private final String modelName;
 
-    public Transport(int wheelesCount, String modelName) {
+    public Transport(String modelName, int wheelesCount) {
         this.wheelsCount = wheelesCount;
         this.modelName = modelName;
     }
@@ -15,5 +15,16 @@ public abstract class Transport {
 
     public String getModelName() {
         return modelName;
+    }
+
+    private void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void service() {
+        System.out.println("Обслуживаем " + this.modelName);
+        for (int i = 0; i < this.wheelsCount; i++) {
+            this.updateTyre();
+        }
     }
 }
--- src/Transport/Truck.java
+++ src/Transport/Truck.java
@@ -1,4 +1,22 @@
 package Transport;
 
-public class Truck {
+public class Truck extends Transport {
+    public Truck(String modelName, int wheelesCount) {
+        super(modelName, wheelesCount);
+    }
+
+    @Override
+    public void service() {
+        super.service();
+        this.checkEngine();
+        this.checkTrailer();
+    }
+
+    private void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    private void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
 }
